28e8cc29bc2d62da97f288b11c12c2649720dd84
Jose Manuel Comber <jmcomber1@gmail.com>
2017-06-24 00:52:14 -0400

    hasta hace un rato funcionaba, ahora algo se rompio pero ir pa atras no funciona

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -41,26 +41,77 @@ QUERIES_FILENAME = 'queries'
 
 @app.route("/", methods=['GET','POST'])
 def home():
-    with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
-        json_file = json.load(queries_file)
-        pairs = [(x["name"],
-                  x["database"],
-                  x["description"],
-                  x["query"]) for x in json_file]
-        return render_template('file.html', results=pairs)
+    if request.method == "GET":
+        with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
+            json_file = json.load(queries_file)
+            pairs = [(x["name"],
+                      x["database"],
+                      x["description"],
+                      x["query"]) for x in json_file]
+            return render_template('file.html', results=pairs)
+    else:
+        with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
+            json_file = json.load(queries_file)
+            pairs = []
+            for x in json_file:
+                if "PARAMETER1" in x["query"]:
+                    dia = request.form['dia']
+                    mes = request.form['mes']
+                    ano = request.form['ano']
+                    mes = str(mes)
+                    if len(mes) < 2:
+                        mes = "0" + mes
+                    dia = str(dia)
+                    if len(dia) < 2:
+                        dia = "0" + dia
+                    ano = str(ano)
+                    param = "{}-{}-{}".format(ano, mes, dia)
+                    # print(param)
+                    pairs.append((x["name"],
+                      x["database"],
+                      x["description"],
+                      param.join(x["query"].split("PARAMETER1"))))
+                    # print(x["query"])
+                    # print(param.join(x["query"].split("PARAMETER1")))
+                    print(pairs[-1][-1])
+                elif "PARAMETER2" in x["query"]:
+                    param = request.form['text']
+                    param2 = request.form['text2']
+                    aux = param.join(x["query"].split("PARAMETER2"))
+                    aux = param2.join(aux.split("PARAMETER3"))
+                    pairs.append((x["name"],
+                      x["database"],
+                      x["description"],
+                      aux))
+                else:
+                    param = request.form['text3']
+                    pairs.append((x["name"],
+                      x["database"],
+                      x["description"],
+                      param.join(x["query"].split("PARAMETER3"))))
+
+
+            # pairs = [(x["name"],
+            #           x["database"],
+            #           x["description"],
+            #           param.join(x["query"].split("PARAMETER1"))) for x in json_file]
+            return render_template('file.html', results=pairs)
 
 
 @app.route("/mongo", methods=['GET','POST'])
 def mongo():
     query = request.args.get("query")
-    query = query.split("PARAMETER")
-    print(query)
-    try: #lo que debería pasar pero no pasa, porque form no tiene text, que está en file.html...
-        param = request.form['text']
-        query = param.join(query)
-    except KeyError: #el caso pa que no falle
-        print("YOLO")
-        query = "2016-10-24".join(query)
+    if "PARAMETER1" in query:
+        query = query.split("PARAMETER1")
+        print(query)
+        try: #lo que debería pasar pero no pasa, porque form no tiene text, que está en file.html...
+            param = request.form['text']
+            # print(param)
+            query = param.join(query)
+        except KeyError: #el caso pa que no falle
+            print("YOLO")
+            query = "2016-10-24".join(query)
+
     results = eval('mongodb.'+query)
     results = json_util.dumps(results, sort_keys=True, indent=4)
     if "find" in query:
